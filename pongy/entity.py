98283f330406264154a534e5aa287a8d5027a33b
Andrew Dobrich <adobrich@internode.on.net>
2017-04-16 22:38:51 +1000

    Refactor complete.

--- pongy/entity.py
+++ pongy/entity.py
@@ -1,14 +1,13 @@
-import settings
-
 import random
+import math
 
 import pyglet
 
 
 class Drawable(object):
-    """Base class for drawable objects."""
+    """Base class for all drawable objects."""
     def __init__(self, x=0, y=0, z=0, rx=0, ry=0, rz=0,
-                 color=[1.0, 1.0, 1.0, 1.0]):
+             color=[1.0, 1.0, 1.0, 1.0]):
         # Position
         self.x = x
         self.y = y
@@ -21,19 +20,12 @@ class Drawable(object):
 
         self.color = color
 
-        self.render_queue = pyglet.graphics.Batch()
-
     def set_properties(self):
-        """Reset matrix then set objects position,
-        rotation and colour in preparation for drawing.
-        """
-        # Reset matrix
+        """Apply position, rotation and color."""
         pyglet.gl.glLoadIdentity()
 
-        # Set position
         pyglet.gl.glTranslatef(self.x, self.y, self.z)
 
-        # Set rotation
         pyglet.gl.glRotatef(self.rx, 1, 0, 0)
         pyglet.gl.glRotatef(self.ry, 0, 1, 0)
         pyglet.gl.glRotatef(self.rz, 0, 0, 1)
@@ -43,34 +35,36 @@ class Drawable(object):
 
 class Rectangle(Drawable):
     """Create a rectangle with provided size and location."""
-    def __init__(self, width, height, x, y):
-        """Inits Rectangle with size and (x, y) coordinates."""
+    def __init__(self, x, y, width, height):
         super(Rectangle, self).__init__(x, y)
         self.half_width = width / 2
         self.half_height = height / 2
 
-        self.render_queue.add(4, pyglet.gl.GL_QUADS, None, ('v2f', (
-                              -self.half_width, self.half_height,
-                              self.half_width, self.half_height,
-                              self.half_width, -self.half_height,
-                              -self.half_width, -self.half_height)))
-
     def draw(self):
         """Render rectangle."""
         self.set_properties()
-        self.render_queue.draw()
+        pyglet.graphics.draw(4, pyglet.gl.GL_QUADS, ('v2f', (
+                             -self.half_width, self.half_height,
+                             self.half_width, self.half_height,
+                             self.half_width, -self.half_height,
+                             -self.half_width, -self.half_height)))
 
 
 class Table(Drawable):
     """Draw the pongy table; it's just a dashed line down the middle
     of the screen.
     """
-    def __init__(self, width=800, height=500, gutter_width=100):
-        """Inits Table with default values."""
+    def __init__(self, width, height, gutter_width=100):
         super(Table, self).__init__()
         self.width = width
         self.height = height
         self.gutter_width = gutter_width
+
+        # Dummy objects along table boundaries for collision detection
+        self.top = CollisionZone(self.width / 2, self.height + 50, self.width, 100)
+        self.bottom = CollisionZone(self.width / 2, -50, self.width, 100)
+        self.left = CollisionZone(-50, self.height / 2, 100, self.height)
+        self.right = CollisionZone(self.width + 50, self.height / 2, 100, self.height)
 
         # We want 30 dashed lines to represent the net
         self.dash_count = 30
@@ -79,12 +73,25 @@ class Table(Drawable):
         self.dash_end = self.dash_length + self.dash_length / 2
         self.dash_width = 2
 
+        self.render_queue = pyglet.graphics.Batch()
+
         for i in range(self.dash_count):
             self.render_queue.add(2, pyglet.gl.GL_LINES, None, ('v2f', (
                                   self.width / 2, self.dash_start,
                                   self.width / 2, self.dash_end)))
             self.dash_start += 2 * self.dash_length
             self.dash_end += 2 * self.dash_length
+
+    def set_game_mode(self):
+        """Move left and right collision zone to allow ball to leave
+        the screen."""
+        self.left = CollisionZone(-150, self.height / 2, 100, self.height * 2)
+        self.right = CollisionZone(self.width + 150, self.height / 2, 100, self.height * 2)
+
+    def set_demo_mode(self):
+        """Prevent ball from leaving the screen"""
+        self.left = CollisionZone(-50, self.height / 2, 100, self.height)
+        self.right = CollisionZone(self.width + 50, self.height / 2, 100, self.height)
 
     def draw(self):
         """Render table."""
@@ -97,55 +104,79 @@ class Table(Drawable):
         pyglet.gl.glPopAttrib()
 
 
-class Paddle(Rectangle):
-    """Paddle that represents the player."""
-    def __init__(self, player, table, width=8, height=40):
-        """Inits Paddle with default values."""
-        if player == settings.Player.PLAYER_1:
-            self.x = table.gutter_width
-            self.y = table.height / 2
-        if player == settings.Player.PLAYER_2 or player == settings.Player.AI:
-            self.x = table.width - table.gutter_width
-            self.y = table.height / 2
-        super(Paddle, self).__init__(width, height, self.x, self.y)
-        self.half_width = width / 2
-        self.half_height = height / 2
-        # Initial velocity
-        self.velocity = 0
-
-    def move_up(self):
-        self.velocity = 10
-
-    def move_down(self):
-        self.velocity = -10
-
-    def stop(self):
-        self.velocity = 0
-
-
 class Ball(Rectangle):
     """Create a ball (square) with provided size and location."""
-    def __init__(self, direction, width=8, height=8):
-        self.x = settings.window_width / 2
-        self.y = settings.window_height / 2
-        super(Ball, self).__init__(width, height, self.x, self.y)
-        self.direction = direction
-        if self.direction == settings.Direction.RIGHT:
-            self.vel_x = random.randrange(3, 7)
-            self.vel_y = random.randrange(-5, 7)
-        else:
-            self.vel_x = -random.randrange(3, 7)
-            self.vel_y = random.randrange(-5, 7)
+    def __init__(self, x, y, width=8, height=8):
+        super(Ball, self).__init__(x, y, width, height)
+        self.x = x
+        self.y = y
+        self.half_width = width / 2
+        self.half_height = height / 2
+        self.vel_x = 0
+        self.vel_y = 0
 
-    def bounce_x(self):
+    def move(self, dt):
+        """Move ball along current vector."""
+        self.x += self.vel_x * dt
+        self.y += self.vel_y * dt
+
+    def wall_bounce(self, direction):
+        """Bounce/reflect ball off walls."""
+        if direction == 'y':
+            self.vel_y *= -1
+        else:
+            # Only when game is over
+            self.vel_x *= -1
+
+    def paddle_bounce(self):
+        """Bounce ball off paddle."""
+        # TODO: Change this so it will rebound the ball back based on
+        # when the ball struck the paddle. Above the center returns the
+        # ball with a positive y, below the center returns with a -y.
+        # P |/  y=+++
+        # A |/  y=++
+        # D |/  y=+
+        # D |-  y=0
+        # L |\  y=-
+        # E |\  y=--
+        # . |\  y=---
         self.vel_x *= -1
 
-    def bounce_y(self):
-        self.vel_y *= -1
+
+    def reset(self, x, y):
+        """Reset ball to (x, y) co-ordinates and serve in random direction."""
+        self.x = x
+        self.y = y
+        self.vel_x = random.randrange(150, 250) * random.choice([-1, 1])
+        self.vel_y = random.randrange(50, 150) * random.choice([-1, 1])
+
+
+class Paddle(Rectangle):
+    """Paddle that represents the player."""
+    def __init__(self, x, y, width=8, height=50):
+        super(Paddle, self).__init__(x, y, width, height)
+        self.x = x
+        self.y = y
+        self.velocity = 0
+        self.half_width = width / 2
+        self.half_height = height / 2
+        self.speed = 450
 
     def update(self, dt):
-        self.x += self.vel_x
-        self.y += self.vel_y
+        """Update location of paddle."""
+        self.y += self.velocity * dt
+
+    def move_up(self):
+        """Move paddle up."""
+        self.velocity = self.speed
+
+    def move_down(self):
+        """Move paddle down."""
+        self.velocity = -self.speed
+    
+    def stop(self):
+        """Stop paddle."""
+        self.velocity = 0;
 
 
 class Score(Drawable):
@@ -153,7 +184,6 @@ class Score(Drawable):
     def __init__(self, x, y,
                  score=0, pixel_size=6, grid_width=9, grid_height=8):
         super(Score, self).__init__()
-        """Inits Score with default values and (x, y) coordinates."""
         self.score = score
         self.pixel_size = pixel_size
         self.grid_anchor = ((grid_width / 2) * pixel_size,
@@ -169,13 +199,19 @@ class Score(Drawable):
                 self.x += pixel_size
             self.y -= pixel_size
             self.x = x - self.grid_anchor[0]
-        self.score_render_queue = self.get_mask()
+        self.update_mask()
 
-    def update(self):
+    def increment(self):
+        """Increase score by one."""
         self.score += 1
-        self.score_render_queue = self.get_mask()
+        self.update_mask()
 
-    def get_mask(self):
+    def reset(self):
+        """Reset score to zero."""
+        self.score = 0
+        self.update_mask()
+
+    def update_mask(self):
         """Fetches the bit mask for the provided score value.
         Only values 0 to 11 (inclusive) are available.
         """
@@ -277,15 +313,34 @@ class Score(Drawable):
                      [0, 0, 0, 1, 0, 0, 1, 0, 0],
                      [0, 0, 0, 1, 0, 0, 1, 0, 0]]
                }
-        render_queue = []
+        self.render_queue = pyglet.graphics.Batch()
         for pos_row, mask_row in zip(self.score_grid, mask[self.score]):
             for pos, mask in zip(pos_row, mask_row):
                 if mask:
-                    render_queue.append(Rectangle(self.pixel_size,
-                                                  self.pixel_size, pos[0], pos[1]))
-        return render_queue
+                    self.render_queue.add(4, pyglet.gl.GL_QUADS, None, ('v2f', (
+                                          pos[0] - self.pixel_size / 2,
+                                          pos[1] + self.pixel_size / 2,
+                                          pos[0] + self.pixel_size / 2,
+                                          pos[1] + self.pixel_size / 2,
+                                          pos[0] + self.pixel_size / 2,
+                                          pos[1] - self.pixel_size / 2,
+                                          pos[0] - self.pixel_size / 2,
+                                          pos[1] - self.pixel_size / 2)))
 
     def draw(self):
-        """Render scoreboard."""
-        for pixel in self.score_render_queue:
-            pixel.draw()
+        """Batch render the scoreboard."""
+        self.render_queue.draw()
+
+
+class CollisionZone(Drawable):
+    """Creates a zone of size width * height centered at (x, y)
+    which other objects can collide with. Useful for creating invisible
+    boundaries."""
+    def __init__(self, x, y, width, height):
+        super(CollisionZone, self).__init__()
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.half_width = width / 2
+        self.half_height = height / 2
---
+++ pongy/pongy.py
@@ -0,0 +1,136 @@
+#!/usr/bin/env python
+import pyglet
+
+from enum import Enum
+
+import entity
+
+
+class Game(pyglet.window.Window):
+    """Initialise and run the Pongy game."""
+    def __init__(self, *args, **kwargs):
+        super(Game, self).__init__(*args, **kwargs)
+        self.table = entity.Table(self.width, self.height)
+        self.ball = entity.Ball(self.width / 2, self.height / 2, 8, 8)
+        self.score_one = entity.Score(self.width * 0.25, self.height)
+        self.score_two = entity.Score(self.width * 0.75, self.height)
+        self.game_over()
+
+    def game_over(self):
+        """End game, and bounce the ball around the screen while waiting for
+        players to start a new game."""
+        self.game_in_progress = False
+        self.table.set_demo_mode()
+        self.ball.reset(self.width / 2, self.height / 2)
+
+    def new_game(self):
+        """Reset score, player/ball positions and start a new game."""
+        self.game_in_progress = True
+        self.table.set_game_mode()
+        self.score_one.reset()
+        self.score_two.reset()
+        self.ball.reset(self.width / 2, self.height / 2)
+        self.paddle_one = entity.Paddle(self.table.gutter_width,
+                                        self.height / 2, 8, 50)
+        self.paddle_two = entity.Paddle(self.width - self.table.gutter_width,
+                                        self.height / 2, 8, 50)
+
+
+    def update(self, dt):
+        """Update game objects and check for collisions."""
+        self.ball.move(dt)
+
+        # Check for ball / paddle collision
+        if self.game_in_progress:
+            # Check win condition
+            if self.score_one.score == 11 or self.score_two.score == 11:
+                self.game_over()
+
+            # Check lose condition
+            if self.has_collided(self.ball, self.table.right):
+                self.score_one.increment()
+                self.ball.reset(self.width / 2, self.height / 2)
+            elif self.has_collided(self.ball, self.table.left):
+                self.score_two.increment()
+                self.ball.reset(self.width / 2, self.height / 2)
+
+            # Update paddles
+            # TODO: check for collision on the _next_ move to prevent
+            # paddle from getting stuck
+            self.paddle_one.update(dt)
+            self.paddle_two.update(dt)
+            if (self.has_collided(self.paddle_one, self.ball) or
+                self.has_collided(self.paddle_two, self.ball)):
+                self.ball.paddle_bounce()
+            if (self.has_collided(self.paddle_one, self.table.top) or
+                self.has_collided(self.paddle_one, self.table.bottom)):
+                self.paddle_one.stop()
+            elif (self.has_collided(self.paddle_two, self.table.top) or
+                  self.has_collided(self.paddle_two, self.table.bottom)):
+                self.paddle_two.stop()
+
+        # Bounce of top and bottom of screen
+        if (self.has_collided(self.ball, self.table.top) or
+            self.has_collided(self.ball, self.table.bottom)):
+            self.ball.wall_bounce('y')
+
+        # Bounce ball of left and right walls if game is not in progress
+        if not self.game_in_progress:
+            if (self.has_collided(self.ball, self.table.left) or
+                self.has_collided(self.ball, self.table.right)):
+                self.ball.wall_bounce('x')
+
+    def on_key_press(self, symbol, modifiers):
+        """Keyboard event handler for key press."""
+        if symbol == pyglet.window.key.ESCAPE:
+            exit(0)
+        elif symbol == pyglet.window.key.SPACE:
+            if not self.game_in_progress:
+                self.new_game()
+        if self.game_in_progress:
+            if symbol == pyglet.window.key.A:
+                self.paddle_one.move_up()
+            elif symbol == pyglet.window.key.Z:
+                self.paddle_one.move_down()
+            elif symbol == pyglet.window.key.UP:
+                self.paddle_two.move_up()
+            elif symbol == pyglet.window.key.DOWN:
+                self.paddle_two.move_down()
+
+    def on_key_release(self, symbol, modifiers):
+        """Keyboard event handler for key release."""
+        if self.game_in_progress:
+            if symbol == pyglet.window.key.A:
+                self.paddle_one.stop()
+            elif symbol == pyglet.window.key.Z:
+                self.paddle_one.stop()
+            elif symbol == pyglet.window.key.UP:
+                self.paddle_two.stop()
+            elif symbol == pyglet.window.key.DOWN:
+                self.paddle_two.stop()
+
+    def on_draw(self):
+        """Render game objects."""
+        self.clear()
+        self.table.draw()
+        self.score_one.draw()
+        self.score_two.draw()
+        self.ball.draw()
+        # Only draw paddles if game is in progress
+        if self.game_in_progress:
+            self.paddle_one.draw()
+            self.paddle_two.draw()
+
+    def has_collided(self, one, two):
+        """Check for collision between two objects using AABB method."""
+        self.x_collision = (one.x + one.half_width >= two.x - two.half_width and
+                            two.x + two.half_width >= one.x - one.half_width)
+        self.y_collision = (one.y + one.half_height >= two.y - two.half_height and
+                            two.y + two.half_height >= one.y - one.half_height)
+        return self.x_collision and self.y_collision
+
+
+if __name__ == '__main__':
+    game = Game(caption='Pongy', width=800, height=500, fullscreen=False)
+    pyglet.clock.schedule_interval(game.update, 1.0 / 60.0)
+    pyglet.app.run()
